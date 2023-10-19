93df046b5ac30f2ed303975d1c3a8b8708869465
Fernando224-por <andresfernandoxd1591@gmail.com>
2023-10-19 10:42:31 -0500

    auth required from courses

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 import { validateSchema } from '../middlewares/validator.middleware.js'
 import { registerCourse } from '../schemas/course.schema.js'
 import { deleteCourse, getCourse, getCourses, patchCourse, postCourse } from '../controllers/course.controller.js'
+import { authRequired } from '../middlewares/validateToken.js'
 // import { authRequired } from '../middlewares/validateToken.js'
 const router = Router()
-router.get('/getCourses', getCourses)
-router.get('/getCourse', getCourse)
-router.post('/newCourse', validateSchema(registerCourse), postCourse)
-router.patch('/updateCourse/:id', validateSchema(registerCourse), patchCourse)
-router.delete('/deleteCourse/:id', deleteCourse)
+router.get('/getCourses', authRequired, getCourses)
+router.get('/getCourse', authRequired, getCourse)
+router.post('/newCourse', authRequired, validateSchema(registerCourse), postCourse)
+router.patch('/updateCourse/:id', authRequired, validateSchema(registerCourse), patchCourse)
+router.delete('/deleteCourse/:id', authRequired, deleteCourse)
 export default router
